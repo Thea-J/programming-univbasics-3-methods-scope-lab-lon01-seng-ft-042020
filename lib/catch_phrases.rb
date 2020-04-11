4a1bd36e460e53a71b8299e29ef517b5db186401
Dorothy John <dorothyy.john@gmail.com>
2020-04-11 00:23:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,11 @@
+#Define the local variable phrase
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
+#Define the local variable status
 def toadstool
+   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
